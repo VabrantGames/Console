9563eed7ebd89b6051d13d4df17c3c10daf71e8e
John <john.developer.mail@gmail.com>
2019-11-25 21:15:33 -0800

    init

---
+++ console/src/main/java/com/vabrant/console/Console.java
@@ -0,0 +1,9 @@
+package com.vabrant.console;
+
+public class Console {
+
+	public Console() {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ test/src/main/java/com/vabrant/console/test/TestLauncher.java
@@ -0,0 +1,9 @@
+package com.vabrant.console.test;
+
+public class TestLauncher {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
